c622278617d341f6e61e58e1e25d21e56697dc93
elhamakbariii <94735414+elhamakbariii@users.noreply.github.com>
2022-02-21 12:23:02 +0330

    upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Tehran" />
+
+        <footer>
+          This project was coded by <em>Elham Akbari</em> and is{" "}
+          <a
+            href="https://github.com/elhamakbariii/react-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
